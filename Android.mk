a62937a7b48da49a7781ddc698f530f4785df2fb
nuclearmistake <nuclearmistake@gmail.com>
2015-03-14 10:09:20 -0400

    android-support-v7 is not required

--- Android.mk
+++ Android.mk
@@ -2,10 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v7-recyclerview
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-res_dir := res ../../../prebuilts/sdk/current/support/v7/appcompat/res
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
 LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := IndecentXposure
 include $(BUILD_PACKAGE)
