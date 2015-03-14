b3acdc2c673f4686d924da69717206d476cfed77
nuclearmistake <nuclearmistake@gmail.com>
2015-03-14 10:29:35 -0400

    Revert "Keep it simple, stupid"
    This reverts commit 6b1534f524e257dd786591e22cb0a07c9b9ff43c.

--- Android.mk
+++ Android.mk
@@ -7,3 +7,14 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := IndecentXposure
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := IndecentXposure
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v7-recyclerview
+LOCAL_SRC_FILES := $(call all-java-files-under, src debug/src)
+res_dir := res debug/res ../../../prebuilts/sdk/current/support/v7/appcompat/res
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
+LOCAL_SDK_VERSION := current
+LOCAL_PACKAGE_NAME := IndecentXposureDebug
+include $(BUILD_PACKAGE)
